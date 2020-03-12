e1099b7c4ca2100c0d89f95d0ced583a7c704ceb
Peter Bellström <peter.bellstrom@kau.se>
2020-03-12 12:23:33 +0100

    Uppdaterat ex6.php

--- Forelasning1/ex6.php
+++ Forelasning1/ex6.php
@@ -18,9 +18,48 @@
 				<input type="submit" name="btnDemo" value="Demonstration" />
 				<input type="hidden" name="hidDemo" value="Alltid med..." />
 			</form>
-			
+			<!-- Under föreläsning 2 kommer mer om attributen action och method -->
 			<?php
 				//Här kommer koden!
+
+				//Inkluderar extern klass som finns i underkatalogen include
+				include("include/OneDice.php");
+
+				//Skapar variabeln oOneDice - Observera $ (dollartecken)
+				//Instansierar ett objekt ur klassen OneDice (som finns i filen OneDice.php och 
+				//skickar med heltalet 6.)
+				$oOneDice = new OneDice(6);
+
+				//Skriver ut resultatet från metodanropet getNbr() från objektet oOneDice (observer $ och ->
+				echo("<p>" . $oOneDice->getNbr() . "</p>"); //Utskriften blir 6
+
+				//anropar metoden setNbr() och skickar med heltalet 99!
+				$oOneDice->setNbr(99);
+
+				//Skriver ut resultatet från metodanropet getNbr() från objektet oOneDice (observer $ och ->
+				echo("<p>" . $oOneDice->getNbr() . "</p>"); //Utskriften blir 1! Varför?
+
+				//includerar extern klass
+				include("include/SixDices.php");
+
+				//Skapar variabel och instansierar objekt
+				//Observera att vi först måste inkludera OneDice.php eftersom SixDices.php använder denna.
+				$oSixDices = new SixDices();
+
+				//Kasta tärningarna
+				$oSixDices->rollDices();
+
+				//Dumpa ut innehållet i vektorn i objectet $oSixDices
+				$oSixDices->dumpDices();
+
+				//Skriver ut summan av alla tärningarna
+				//Observera echo() eftersom metoden returnerar värdet.
+				//Observera också punkten (.) som utgör strängkontenering (är + i JavaScipt)
+				echo( "<p>Summan är " . $oSixDices->sumDices() . "!</p>");
+
+				//Skriver ut tärningarna som bilder (svg).
+				//Observera echo() eftersom metoden returnerar värdet.
+				echo( $oSixDices->svgDices() );
 			?>
 
 		</div>
