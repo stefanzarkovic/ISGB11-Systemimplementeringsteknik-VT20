328ebaa36460d4702005913c22f03a28ada7f4eb
Peter Bellström <peter.bellstrom@kau.se>
2020-03-13 12:12:19 +0100

    Uppdaterat ex3.php

--- Forelasning2/ex3.php
+++ Forelasning2/ex3.php
@@ -6,6 +6,50 @@
 		<style>
 			<?php
 				//Här kommer koden...
+
+				//Flagga för om knappen skall vara användbar eller inte
+				$disabled = true;
+
+				//difaultvärde för CSS
+				$css = "body { color: rgb(0,0,0); background-color: rgb(255,255,255); }";
+
+				if( isset( $_POST["btnSend"])) {
+
+
+					$fgColor =  $_POST["foregroundcolor"]; 
+					$bgColor = $_POST["backgroundcolor"];
+
+					setcookie( "fgColor", $fgColor, time() + 3600);
+					setcookie( "bgColor", $bgColor, time() + 3600);
+
+					$css = "body{ color: $fgColor; background-color: $bgColor; }";
+					
+					$disabled = false;
+
+				}
+
+				if( isset( $_POST["btnReset"])) {
+					setcookie( "fgColor", "", time() - 3600);
+					setcookie( "bgColor", "", time() - 3600);
+				}
+
+				if(!isset($_POST["btnSend"]) && 
+					!isset($_POST["btnReset"]) && 
+					isset($_COOKIE["fgColor"]) && 
+					isset($_COOKIE["bgColor"])) {
+
+					$fgColor = $_COOKIE["fgColor"];
+					$bgColor = $_COOKIE["bgColor"];
+
+					$css = "body { color: $fgColor; background-color: $bgColor; }";
+
+					$disabled = false;
+
+				}
+				
+
+				echo($css);
+
 			?>
 		</style>
 	</head>
@@ -27,7 +71,7 @@
 				</select>
 			
 				<input type="submit" name="btnSend" value="Send" />
-				<input type="submit" name="btnReset" value="Reset" />
+				<input type="submit" name="btnReset" value="Reset" <?php if($disabled) { echo("disabled"); } ?>/>
 			
 			</form>
 		
